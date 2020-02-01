619525ec776efa625bd050010aa3de8973f655c2
SeferAlen <sefer.alen@yahoo.com>
2020-02-01 20:27:24 +0100

    BasicContent: Menu and test content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'Home', 
+  component: HomeComponent
+  }, {
+  path: 'About',
+  component: AboutComponent
+  }, {
+  path: 'Products', 
+  component: ProductsComponent
+  }, {
+  path: 'Contact', 
+  component: ContactComponent
+  }, { 
+  path: '**', 
+  redirectTo: 'Home' 
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './Components/top-navbar/top-navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
