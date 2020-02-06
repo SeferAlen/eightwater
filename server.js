c8a119adc67b046fd04290a9c574a8f527f28a2f
SeferAlen <sefer.alen@yahoo.com>
2020-02-06 13:44:40 +0100

    Heroku: Prepare Heroku deploy fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Billy'));
+app.use(express.static(__dirname + '/dist/eightwater'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Billy/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/eightwater/index.html'));
 });
 
 // default Heroku PORT
